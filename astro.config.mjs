4f71073362d10ec2f34e5ef0b4b61168a2901b60
jrdelrio <jrdelriodom@gmail.com>
2025-08-11 17:00:14 -0400

    cambiado congif a adapter: vercel()

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel"; 
+
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [react()],
-    adapter: node({ mode: "standalone" }),
+    // adapter: node({ mode: "standalone" }),
+    adapter: vercel(),
     output: "server",
 });
