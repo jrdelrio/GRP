50270f6d797f67357ab3ef57c5e2d04792763a5e
jrdelrio <jrdelriodom@gmail.com>
2025-09-11 19:12:14 +0200

    agregado el paquete astro-sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel"; 
+import vercel from "@astrojs/vercel";
+
+
+import sitemap from "@astrojs/sitemap";
 
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
+    integrations: [react(), sitemap()],
     // adapter: node({ mode: "standalone" }),
     adapter: vercel(),
     output: "server",
